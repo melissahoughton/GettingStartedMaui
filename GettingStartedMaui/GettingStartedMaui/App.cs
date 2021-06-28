df2410766f4b828b66edf7ba6826bc90ac10a2d6
Melissa Houghton <57604950+melissahoughton@users.noreply.github.com>
2021-06-28 16:32:39 +1000

    Change starter template from XAML to C# (#1)
    * Initial conversion from XAML to C#
    
    * Add image and background color
    
    * Add colors, padding, and update code formatting

---
+++ GettingStartedMaui/GettingStartedMaui/App.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+using System.Collections.Generic;
+
+namespace GettingStartedMaui
+{
+    public class App : IApplication
+    {
+		List<IWindow> _windows = new List<IWindow>();
+		public IReadOnlyList<IWindow> Windows => _windows.AsReadOnly();
+
+		public App(IImageSourceServiceConfiguration imageConfig)
+        {
+            imageConfig.SetImageDirectory("Assets");
+        }
+
+		public IWindow CreateWindow(IActivationState activationState)
+		{
+			var window = new Window(new MainPage());
+			_windows.Add(window);
+			return window;
+		}
+	}
+}
